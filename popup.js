28d9f2c6ce7e4dfc3861a3a31b899f7f8209c93f
TopVirusMinus <mwmma5000@gmail.com>
2022-12-25 18:10:11 +0200

    added copy to clipboard with feedback button

--- popup.js
+++ popup.js
@@ -22,6 +22,33 @@ function displayPlaylist(playlist) {
   document.getElementById("playlist-container").innerHTML = "";
   var ol = document.createElement("ol");
 
+  var button = document.createElement("button");
+  button.textContent = "Copy to Clipboard";
+  button.addEventListener("click", () => copyFeedback());
+
+  // Join the playlist into a single string, with a breakline after each video
+  button.addEventListener("click", function () {
+    // Join the playlist into a single string
+    var text = playlist
+      .map(function (video, index) {
+        return `${index + 1}. ${video}`;
+      })
+      .join("\n");
+
+    // Copy the text to the clipboard
+    navigator.clipboard.writeText(text);
+  });
+  document.getElementById("playlist-container").appendChild(button);
+
+  function copyFeedback() {
+    var feedback = document.getElementById("feedback");
+    feedback.classList.add("show");
+
+    // Hide the feedback message after 3 seconds
+    setTimeout(function () {
+      feedback.classList.remove("show");
+    }, 1000);
+  }
   // Loop through the playlist and create a list item for each video
   playlist.forEach(function (video) {
     var li = document.createElement("li");
